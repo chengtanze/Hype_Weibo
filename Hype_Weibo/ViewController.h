9e2b98436acd711f1b776d7ebcde368970e626e3
chengtanze <cheng@chengdeMacBook-Pro.local>
2015-01-07 22:54:40 +0800

    weibo

---
+++ Hype_Weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hype_Weibo
+//
+//  Created by cheng on 15/1/7.
+//  Copyright (c) 2015年 cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
