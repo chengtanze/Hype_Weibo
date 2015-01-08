7d306ffa0378017ef71f5cb46e7d294f92adbdce
chengtanze <cheng@chengdeMacBook-Pro.local>
2015-01-08 21:45:24 +0800

    获取微博协议

--- Hype_Weibo/ViewController.m
+++ Hype_Weibo/ViewController.m
@@ -12,6 +12,12 @@
 #import "WBHttpRequest.h"
 //3965386710
 //https://api.weibo.com/oauth2/default.html
+
+//2.00ZcVhxFKO331Eafe2057080SpkIZE
+//5462837643
+
+//chengtanze@gmail.com
+//Hype@5162291.com
 @interface ViewController ()
 
 @property (strong, nonatomic) NSString *wbtoken;
@@ -26,6 +32,10 @@
 
     [WeiboSDK enableDebugMode:YES];
     [WeiboSDK registerApp:@"3965386710"];
+    
+    self.wbCurrentUserID = @"5462837643";
+    self.wbtoken = @"2.00ZcVhxFKO331Eafe2057080SpkIZE";
+
 }
 //https://api.weibo.com/oauth2/authorize?client_id=3965386710&response_type=code&redirect_uri=https://api.weibo.com/oauth2/default.html
 
@@ -68,15 +78,50 @@
                              @"Other_Info_2": @[@"obj1", @"obj2"],
                              @"Other_Info_3": @{@"key1": @"obj1", @"key2": @"obj2"}};
         BOOL sucss = [WeiboSDK sendRequest:request];
-        NSLog(@"sucss:%ld",sucss);
+    
 }
 
 - (IBAction)GetFans:(id)sender {
     
+//    [WBHttpRequest requestForRenewAccessTokenWithRefreshToken:@"discussion" queue:nil withCompletionHandler:^(WBHttpRequest *httpRequest, id result, NSError *error) {
+//        DemoRequestHanlder(httpRequest, result, error);
+//    }];
+    
     AppDelegate *myDelegate =(AppDelegate*)[[UIApplication sharedApplication] delegate];
-    [WBHttpRequest requestForFollowersListOfUser:myDelegate.wbCurrentUserID withAccessToken:myDelegate.wbtoken andOtherProperties:nil queue:nil withCompletionHandler:^(WBHttpRequest *httpRequest, id result, NSError *error) {
+    [WBHttpRequest requestForFollowersListOfUser:self.wbCurrentUserID withAccessToken:self.wbtoken andOtherProperties:nil queue:nil withCompletionHandler:^(WBHttpRequest *httpRequest, id result, NSError *error) {
         
         DemoRequestHanlder(httpRequest, result, error);
+        
+    }];
+    
+//    [WBHttpRequest requestForStatusIDsFromCurrentUser:myDelegate.wbCurrentUserID withAccessToken:myDelegate.wbtoken andOtherProperties:nil queue:nil withCompletionHandler:^(WBHttpRequest *httpRequest, id result, NSError *error) {
+//        
+//        DemoRequestHanlder(httpRequest, result, error);
+//    }];
+    
+//    [WBHttpRequest requestForFriendsListOfUser:myDelegate.wbCurrentUserID withAccessToken:myDelegate.wbtoken andOtherProperties:nil queue:nil withCompletionHandler:^(WBHttpRequest *httpRequest, id result, NSError *error) {
+//        DemoRequestHanlder(httpRequest, result, error);
+//    }];
+    
+    //NSString *str=[NSString stringWithFormat:@"https://api.weibo.com/2/users/show.json?access_token=%@&uid=%@", myDelegate.wbtoken,myDelegate.wbCurrentUserID];
+    //NSString *str=[NSString stringWithFormat:@"https://api.weibo.com/2/statuses/public_timeline.json?access_token=%@&count=1", self.wbtoken];
+    NSString *str=[NSString stringWithFormat:@"https://api.weibo.com/2/statuses/friends_timeline.json?access_token=%@&count=1", self.wbtoken];
+    
+    NSURL *url = [NSURL URLWithString:[str stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    AFHTTPRequestOperationManager * opearManager = [AFHTTPRequestOperationManager manager];
+    opearManager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];//设置相应内容类型
+    [opearManager GET:str parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSString *html  = operation.responseString;
+        NSData* data=[html dataUsingEncoding:NSUTF8StringEncoding];
+        NSDictionary * dict=[NSJSONSerialization  JSONObjectWithData:data options:0 error:nil];
+        NSLog(@"获取到的数据为：%@",dict);
+        
+        //[self ParseDataFromJson:dict];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"发生错误！%@",error);
+        
+        NSString *html  = operation.responseString;
         
     }];
 }
