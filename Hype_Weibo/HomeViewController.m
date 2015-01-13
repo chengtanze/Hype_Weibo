812ed12ccf43b28dc7677e05e3bc360b98883939
chengtanze <cheng@chengdeMacBook-Pro.local>
2015-01-13 21:40:01 +0800

    tableviewcell

--- Hype_Weibo/HomeViewController.m
+++ Hype_Weibo/HomeViewController.m
@@ -22,36 +22,36 @@
     
     self.Weibo_Home_TableView.delegate = self;
     self.Weibo_Home_TableView.dataSource = self;
-    self.Weibo_Home_TableView.rowHeight = 50;
+    self.Weibo_Home_TableView.rowHeight = 80;
     
-//    self.weiboArray = [[NSMutableArray alloc]init];
-//    
-//    NSDictionary * dicName = @{@"name":@"十万个冷笑话"};
-//    NSDictionary * dicContext = @{@"Context":@"【Dunkin' Donuts将在中国开设1400家新店面】- 这家咖啡连锁店周四宣布了这一计划，在美国销量下滑之际，该公司正在寻求扩张全球业务。Dunkin' Donuts表示，将与Golden Cup Pte.Ltd.合作，由后者负责于未来20年内在中国开设和运营Dunkin' Donuts连锁店。即将在中国开设的新店面将于今年第四季度开张。"};
-//    NSDictionary * dicTime = @{@"time":@"4小时前"};
-//    NSDictionary * dicFrom = @{@"from":@"新浪微博"};
-//
-//    
-//    NSMutableDictionary * dicHeight = [[NSMutableDictionary alloc]initWithObjectsAndKeys:@"0", @"height",nil];
-//    
-//    NSMutableArray * user = [[NSMutableArray alloc]initWithObjects: dicName, dicContext, dicFrom, dicTime, dicHeight,nil];
-//
-//    NSMutableDictionary * dic = [[NSMutableDictionary alloc]initWithObjectsAndKeys:@"123456",@"Key", user, @"data",nil];
-//
-//    [self.weiboArray addObject:dic];
-//    
-//    NSDictionary * dicName1 = @{@"name":@"企业微助理"};
-//    NSDictionary * dicContext1 = @{@"Context":@"#网事早知道#微博在成为品牌营销沟通重要平台的同时，也已在各类危机的爆发、传播和升级中扮演起愈加重要的角色，越来越多的企业将微博作为品牌公关的主战场。"};
-//    NSDictionary * dicTime1 = @{@"time":@"2小时前"};
-//    NSDictionary * dicFrom1 = @{@"from":@"新浪微博"};
-//    
-//    NSMutableDictionary * dicHeight1 = [[NSMutableDictionary alloc]initWithObjectsAndKeys:@"0", @"height",nil];
-//    
-//    NSMutableArray * user1 = [[NSMutableArray alloc]initWithObjects: dicName1, dicContext1, dicFrom1, dicTime1, dicHeight1,nil];
-//    //NSDictionary * dic = @{@"Key":@"123456", @"data":user};
-//    NSMutableDictionary * dic1 = [[NSMutableDictionary alloc]initWithObjectsAndKeys:@"78910",@"Key", user1, @"data",nil];
+    self.weiboArray = [[NSMutableArray alloc]init];
+    
+    NSDictionary * dicName = @{@"name":@"十万个冷笑话"};
+    NSDictionary * dicContext = @{@"Context":@"【Dunkin' Donuts将在中国开设1400家新店面】- 这家咖啡连锁店周四宣布了这一计划，在美国销量下滑之际，该公司正在寻求扩张全球业务。Dunkin' Donuts表示，将与Golden Cup Pte.Ltd.合作，由后者负责于未来20年内在中国开设和运营Dunkin' Donuts连锁店。即将在中国开设的新店面将于今年第四季度开张。"};
+    NSDictionary * dicTime = @{@"time":@"4小时前"};
+    NSDictionary * dicFrom = @{@"from":@"新浪微博"};
 
-//    [self.weiboArray addObject:dic1];
+    
+    NSMutableDictionary * dicHeight = [[NSMutableDictionary alloc]initWithObjectsAndKeys:@"0", @"height",nil];
+    
+    NSMutableArray * user = [[NSMutableArray alloc]initWithObjects: dicName, dicContext, dicFrom, dicTime, dicHeight,nil];
+
+    NSMutableDictionary * dic = [[NSMutableDictionary alloc]initWithObjectsAndKeys:@"123456",@"Key", user, @"data",nil];
+
+    [self.weiboArray addObject:dic];
+    
+    NSDictionary * dicName1 = @{@"name":@"企业微助理"};
+    NSDictionary * dicContext1 = @{@"Context":@"#网事早知道#微博在成为品牌营销沟通重要平台的同时，也已在各类危机的爆发、传播和升级中扮演起愈加重要的角色，越来越多的企业将微博作为品牌公关的主战场。"};
+    NSDictionary * dicTime1 = @{@"time":@"2小时前"};
+    NSDictionary * dicFrom1 = @{@"from":@"新浪微博"};
+    
+    NSMutableDictionary * dicHeight1 = [[NSMutableDictionary alloc]initWithObjectsAndKeys:@"0", @"height",nil];
+    
+    NSMutableArray * user1 = [[NSMutableArray alloc]initWithObjects: dicName1, dicContext1, dicFrom1, dicTime1, dicHeight1,nil];
+    //NSDictionary * dic = @{@"Key":@"123456", @"data":user};
+    NSMutableDictionary * dic1 = [[NSMutableDictionary alloc]initWithObjectsAndKeys:@"78910",@"Key", user1, @"data",nil];
+
+    [self.weiboArray addObject:dic1];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -80,11 +80,13 @@
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    [tableView registerNib:[UINib nibWithNibName:@"Weibo_Home_TableViewCell" bundle:nil] forCellReuseIdentifier:@"Weibo_Home_Cell"];
+    Weibo_Home_TableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"Weibo_Home_Cell"];
     
-//    UITableViewCell * cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Weibo_Home_TableViewCell"];
+    //这个方法加载不能实现 回收机制
+//    Weibo_Home_TableViewCell *cell = [[[NSBundle mainBundle] loadNibNamed:@"Weibo_Home_TableViewCell" owner:self options:nil] objectAtIndex:0];
+
     
-    Weibo_Home_TableViewCell * cell ;
-    cell = [[[NSBundle mainBundle] loadNibNamed:@"Weibo_Home_TableViewCell" owner:self options:nil] objectAtIndex:0];
     NSLog(@"cellForRowAtIndexPath %ld", indexPath.row);
     NSDictionary * dic = self.weiboArray[indexPath.row];
 
